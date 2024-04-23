7de522bad9359fc590b0f1f0e774d351292b8c9c
B1GHEADD <cresti1357@gmail.com>
2024-04-23 09:00:59 +0700

    new

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-blue-200 w-screen h-screen grid">
+    <div class="h-20 w-auto bg-red-300 p-3 flex gap-5">
+      <div class="w-1/5">
+        <p class="bg-yellow-200 align-middle font-bold">LOGO</p>
+      </div>
+      <ul class="flex justify-between bg-blue-500 w-4/5 items-center">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Tools & Technology</li>
+        <li>How We Work</li>
+      </ul>
+      <button class="bg-blue-400 rounded-lg">Get a Quotes</button>
+    </div>
+    <div class="bg-green-300 h-auto">
+      <h1>hello</h1>
+    </div>
+  </div>
+</template>
