9a8dd15f79e1e990fdafd15e1fab719190fcf9bd
B1GHEADD <cresti1357@gmail.com>
2024-04-24 09:02:35 +0700

    .add

--- app.vue
+++ app.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="bg-blue-200 w-screen h-screen grid">
-    <div class="h-20 w-auto bg-red-300 p-3 flex gap-5">
-      <div class="w-1/5">
-        <p class="bg-yellow-200 align-middle font-bold">LOGO</p>
-      </div>
-      <ul class="flex justify-between bg-blue-500 w-4/5 items-center">
-        <li>Home</li>
-        <li>Services</li>
-        <li>Tools & Technology</li>
-        <li>How We Work</li>
+  <div class="p-auto m-autos" style="background-image: url('./asset/img/Bg-Web\ Cleaning.jpg')">
+    <div class="h-20 w-auto bg-white p-3 flex gap-5">
+      <p class="font-bold text-4xl text-blue-500 text-center align-middle w-1/5 my-auto">LOGO</p>
+      <ul class="flex justify-between w-3/5 items-center px-32">
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">Tools & Technology</a>
+        <a href="#">How We Work</a>
       </ul>
-      <button class="bg-blue-400 rounded-lg">Get a Quotes</button>
+      <button class="bg-[#6382F7] rounded-lg text-white px-10 ml-14">Get a Quotes</button>
     </div>
-    <div class="bg-green-300 h-auto">
-      <h1>hello</h1>
-    </div>
+    <div class="bg-cover bg-center" style="background-image: url('./asset/img/Bg-WebCleaning.jpg')"></div>
   </div>
 </template>
